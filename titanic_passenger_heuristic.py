d2f45b0633bf59eef67538ec28cdd951bf1c5972
amandeepsharma89 <amandeepsharma89@gmail.com>
2015-01-01 11:27:54 -0500

    Added some more heuristic
    Added some more complex heuristic to increase the predictability

--- titanic_passenger_heuristic.py
+++ titanic_passenger_heuristic.py
@@ -11,38 +11,95 @@ import numpy as np
 #import statsmodels.api as sm
 import os
 
-nbr_predicted = 0
-# Interpretation: Holds the number of correctly guessed survival rate based on heuristic used
+sex_heu_predicted = 0
+# Interpretation: Holds the number of correctly guessed survival rate based on sex heuristic used
+sex_status_u18_predicted = 0
+# Interpretation: Holds the number of correctly guessed survival rate based on sex or high class ticket and under 18 heuristic used
+complex_predicted = 0
+# Interpretation: Holds the number of correctly guessed survival rate based on complex heuristic used
 titanic_data = "C:/Python27/incoming/Intro-DataSc/titanic_data.csv"
 # Interpretation: Holds the entire titanic dataset
 predictions = {}
 # Interpretation: This dictionary will hold the final heuristic in the form of passenger_id and values
 #				  0 for not survived and 1 survived
+df = pd.read_csv(titanic_data)
 
-# simple_heuristic: DataFrame -> Void
+# sex_heuristic: DataFrame -> Void
 # Input: Passenger DataFrame, it has all the information of Titanic passengers
 # Returns: If passenger is male then the dictionary key(which is the passengerId) us set to 0
 #		   else it is set to 1
-def simple_heuristic(passenger):
+def sex_heuristic(passenger):
 	if(passenger.Sex == 'male'):
 		predictions[passenger.PassengerId] = 0
 	else:
 		predictions[passenger.PassengerId] = 1
 
-# correct_simple_heuristic_prediction: DataFrame -> Void
-# Returns: Increments the correct predictions if simple_heuristic predicts the actual outcome 
-def correct_simple_heuristic_prediction(passenger):
-	global nbr_predicted
+# correct_sex_heuristic_prediction: DataFrame -> Void
+# Returns: Increments the correct predictions if sex_heuristic predicts the actual outcome 
+def correct_sex_heuristic_prediction(passenger):
+	global sex_heu_predicted
 	if predictions[passenger.PassengerId] == passenger.Survived:
-		nbr_predicted += 1
+		sex_heu_predicted += 1
 
-df = pd.read_csv(titanic_data)
+# sex_status_u18_heuristic: DataFrame -> Void
+# Input: Passenger DataFrame, it has all the information of Titanic passengers
+# Returns: If passenger is female or is under 18 and has a high status ticket 
+# then the dictionary key(which is the passengerId) us set to 1		
+def sex_status_u18_heuristic(passenger):
+	if(passenger.Sex == 'female' or (passenger.Pclass == 1 and passenger.Age < 18)):
+		predictions[passenger.PassengerId] = 1
+	else:
+		predictions[passenger.PassengerId] = 0
+		
+# correct_sex_heuristic_prediction: DataFrame -> Void
+# Returns: Increments the correct predictions if sex_status_u18_heuristic predicts the actual outcome 
+def correct_sex_status_u18_heuristic_prediction(passenger):
+	global sex_status_u18_predicted
+	if predictions[passenger.PassengerId] == passenger.Survived:
+		sex_status_u18_predicted += 1
 
-# simple_predictor: Void -> Void
-def simple_predictor():
+# complex_heuristic: DataFrame -> Void
+# Input: Passenger DataFrame, it has all the information of Titanic passengers
+# Returns: Predicts the survival rate based	complex conditions
+def complex_heuristic(passenger):
+	if(((passenger.Sex == 'female' or passenger.Age < 13) and passenger.Pclass < 3)\
+		or (passenger.Pclass == 3 and passenger.Sex == 'female' and passenger.SibSp < 3)
+		or (passenger.Sex == 'male' and passenger.Age < 15 and passenger.Pclass < 3)):
+		predictions[passenger.PassengerId] = 1
+	else:
+		predictions[passenger.PassengerId] = 0
+
+# correct_complex_prediction: DataFrame -> Void
+# Returns: Increments the correct predictions based on complex condition 
+def correct_complex_prediction(passenger):
+	global complex_predicted
+	if predictions[passenger.PassengerId] == passenger.Survived:
+		complex_predicted += 1
+		
+# sex_predictor: Void -> Void
+def sex_predictor():
 	for passenger_index, passenger in df.iterrows():
-		simple_heuristic(passenger)
-		correct_simple_heuristic_prediction(passenger)	
+		sex_heuristic(passenger)
+		correct_sex_heuristic_prediction(passenger)	
 
-simple_predictor()		
-print 'Number of correctly perdicted is %s out of possible %s' %(nbr_predicted, len(df.index))
+# sex_status_u18_predictor: Void -> Void
+def	sex_status_u18_predictor():
+	for passenger_index, passenger in df.iterrows():
+		sex_status_u18_heuristic(passenger)
+		correct_sex_status_u18_heuristic_prediction(passenger)
+
+def complex_predictor():
+	for passenger_index, passenger in df.iterrows():
+		complex_heuristic(passenger)
+		correct_complex_prediction(passenger)
+	
+		
+sex_predictor()
+sex_status_u18_predictor()
+complex_predictor()
+print 'Number of correct predictions using sex heuristic is %s out of possible %s which is %s percent' %(sex_heu_predicted, len(df.index),\
+	   format(sex_heu_predicted*100/len(df.index),'.2f'))
+print 'Number of correct predictions using sex, age and status heuristic is %s out of possible %s which is %s percent' %(sex_status_u18_predicted, \
+	   len(df.index), format(sex_status_u18_predicted*100/len(df.index),'.2f'))
+print 'Number of correct predictions using complex heuristic is %s out of possible %s which is %s percent' %(complex_predicted, \
+	   len(df.index), format(complex_predicted*100/len(df.index),'.2f'))	   
